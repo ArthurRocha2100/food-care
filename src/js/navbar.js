27e5ace21be7c06cfc245f8df481f7c2be9a1a66
ArthurRocha2100 <arthurrocha2101@gmail.com>
2023-11-07 06:07:16 -0300

    implementando menu suspenso

---
+++ src/js/navbar.js
@@ -0,0 +1,9 @@
+const suspendedMenu = document.getElementById('suspended-menu');
+
+function showMenu() {
+    suspendedMenu.style.display = 'block';
+}
+
+function hideMenu() {
+    suspendedMenu.style.display = 'none'
+}
