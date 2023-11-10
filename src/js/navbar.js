39337cc266123066b63fc253932cb9c9eb6fc085
ArthurRocha2100 <arthurrocha2101@gmail.com>
2023-11-09 22:19:05 -0300

    immplementação de header inicial

--- src/js/navbar.js
+++
@@ -1,9 +0,0 @@
-const suspendedMenu = document.getElementById('suspended-menu');
-
-function showMenu() {
-    suspendedMenu.style.display = 'block';
-}
-
-function hideMenu() {
-    suspendedMenu.style.display = 'none'
-}
