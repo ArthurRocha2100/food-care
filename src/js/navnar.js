c1c1dc8dbd7412a21a4656447194a31008dfdee3
ArthurRocha2100 <arthurrocha2101@gmail.com>
2023-11-12 16:45:22 -0300

    Ajunstes no cabeçalho, implementação de seção O que é e melhoria no layout e responsiviade

--- src/js/navnar.js
+++ src/js/navnar.js
@@ -1,34 +1,20 @@
 document.addEventListener('DOMContentLoaded', function (){
     
     var returTopBtn = document.getElementById('return-top');
-    var missionSection = document.getElementById('mission');
-    var aboutSection = document.getElementById('about')
     var header = document.querySelector('header');
 
-    function checkMissionSectionVisibility() {
-        var missionRect = missionSection.getBoundingClientRect();
+    function checkHeaderVisibility() {
+        var headerRect = header.getBoundingClientRect();
 
-        if (missionRect.top <= 6) {
-            returTopBtn.style.display = 'block' 
-            header.style.position = 'static';
-            header.style.top = 0;
-            aboutSection.style.marginTop = 0;
+        if(headerRect.bottom <= 0) {
+            returTopBtn.style.display = 'block'
         }
-        else if (missionRect.top <= 90) {
-            header.style.position = 'absolute';
-            header.style.top = 'calc(100vh)';
-            aboutSection.style.marginTop = '90px';
-
-        } 
-        else {
-            header.style.position = 'sticky';
-            header.style.top = 0;
-            aboutSection.style.marginTop = 0;
+        else{
             returTopBtn.style.display = 'none'
         }
     }
 
-    window.addEventListener('scroll', checkMissionSectionVisibility);
+    window.addEventListener('scroll', checkHeaderVisibility);
     
-    checkMissionSectionVisibility();
+    checkHeaderVisibility();
 })
