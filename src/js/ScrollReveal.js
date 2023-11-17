f452c0a04f29b19a8efb17d7fe318830dc665c3a
ArthurRocha2100 <arthurrocha2101@gmail.com>
2023-11-17 16:20:29 -0300

    dinimuindo tempo de exebição

--- src/js/ScrollReveal.js
+++ src/js/ScrollReveal.js
@@ -1,15 +1,15 @@
 function revealX() {
-    window.sr = ScrollReveal({ duration:2000, distance:'100px', easing:'ease-out'});
+    window.sr = ScrollReveal({ duration:1500, distance:'100px', easing:'ease-out'});
 
-    sr.reveal('.what_is-container', {origin:'top', reset:true});
-    sr.reveal('.allergy-food-img', {origin:'left', reset:true});
+    sr.reveal('.what_is-container', {origin:'top', reset:false});
+    sr.reveal('.allergy-food-img', {origin:'left', reset:false});
 
-    sr.reveal('.manifestation-container', {origin:'left', reset:true});
-    sr.reveal('.manifestation_container-img', {origin:'bottom', reset:true});
-    sr.reveal('.diagnosis-container', {origin:'bottom', reset:true});
-    sr.reveal('.treatment-container', {origin:'bottom', reset:true});
-    sr.reveal('.prevention-container', {origin:'bottom', reset:true});
-    sr.reveal('.main_allergens-item', {origin:'left', reset:true});
+    sr.reveal('.manifestation-container', {origin:'left', reset:false});
+    sr.reveal('.manifestation_container-img', {origin:'bottom', reset:false});
+    sr.reveal('.diagnosis-container', {origin:'bottom', reset:false});
+    sr.reveal('.treatment-container', {origin:'bottom', reset:false});
+    sr.reveal('.prevention-container', {origin:'bottom', reset:false});
+    sr.reveal('.main_allergens-item', {origin:'left', reset:false});
 
 
     
